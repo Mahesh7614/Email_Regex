7525a85ccec35064ec5b226506132e2b053ce37c
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-21 21:37:26 +0530

    Validate abc

---
+++ Email_Regex/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Email_Regex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Email ID : ");
+            string email = Console.ReadLine();
+            Regex_Pattern.Pattern(email);
+        }
+    }
+}
---
+++ Email_Regex/Regex_Pattern.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace Email_Regex
+{
+    internal class Regex_Pattern
+    {
+        public static void Pattern(string pattern)
+        {
+            Regex code = new Regex(@"(abc)");
+            bool matches = code.IsMatch(pattern);
+
+            if (matches == true)
+            {
+                Console.WriteLine("Email is Valid");
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Valid Email");
+            }
+        }
+    }
+}
